c337cea45eed15e7b4405c7e38f00d0f0024b8a2
Joswag Sandoval <sergiojr100@outlook.com>
2022-05-06 16:16:49 -0500

    L

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,20 @@
 
 // Your Code Here
+async function main() {
+    let response = await fetch("http://localhost:3001/listBooks")
+    let books = await response.json()
+
+    console.log(books)
+
+    const showBook = (book) => {
+        let newBook = document.createElement("div")
+        let title = document.createElement("h3")
+        let quantityInput = document.createElement("input")
+        let submitQuantity = document.createElement("button")
+        let deleteBook = document.createElement("button")
+
+
+    }
+
+    books.forEach(showBook)
+}
